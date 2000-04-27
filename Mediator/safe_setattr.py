fd9bdee0e4e308e9bd92cd4bd72ad892f15fd6b1
Alain Désilets <alain.desilets@nrc-cnrc.gc.ca>
2000-04-27 20:24:51 +0000

    - Forgot to include file safe_setattr.py which was required by Object.py

---
+++ Mediator/safe_setattr.py
@@ -0,0 +1,7 @@
+def __setattr__(self, name, value):
+    if (self.__dict__.has_key(name)):
+#        sys.stdout.write("attribute '%s' exists\n" % (name))
+        self.__dict__[name] = value
+    else:
+#           sys.stderr.write("ERROR: Could not SET attribute '" + name + "'\n")
+           throw('attr_set')
