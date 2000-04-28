7df212ec55de4d7472ff68c6cf8e7e2d6bd72a05
Alain Désilets <alain.desilets@nrc-cnrc.gc.ca>
2000-04-28 17:09:27 +0000

    - Fixed inline documentation and fixed self-test for Object.py

--- Mediator/safe_setattr.py
+++ Mediator/safe_setattr.py
@@ -1,7 +1,9 @@
+
 def __setattr__(self, name, value):
+#    sys.stdout.write('calling safe __setattr__\n')
     if (self.__dict__.has_key(name)):
 #        sys.stdout.write("attribute '%s' exists\n" % (name))
         self.__dict__[name] = value
     else:
-#           sys.stderr.write("ERROR: Could not SET attribute '" + name + "'\n")
-           throw('attr_set')
+#        sys.stderr.write("ERROR: Could not SET attribute '" + name + "'\n")
+        throw('attr_set')
