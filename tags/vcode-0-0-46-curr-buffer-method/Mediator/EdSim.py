7b2f7093b32b31855471f8118bb10dba6a2f0b10
nobody <nobody@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2001-10-07 20:43:05 +0000

    This commit was manufactured by cvs2svn to create tag 'vcode-0-0-46-curr-buffer-method'.

---
+++ tags/vcode-0-0-46-curr-buffer-method/Mediator/EdSim.py
@@ -0,0 +1,105 @@
+##############################################################################
+# VoiceCode, a programming-by-voice environment
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# (C)2000, National Research Council of Canada
+#
+##############################################################################
+
+"""VoiceCode editor simulator."""
+
+import os, posixpath, re, sys
+import auto_test, debug
+import AppState
+from SourceBuffEdSim import SourceBuffEdSim
+
+class EdSim(AppState.AppState):
+    """VoiceCode editor simulator.
+
+    This class is used to simulate an external programming editor.
+
+    Useful for debugging VoiceCode mediator in isolation from external editor.
+    
+    **INSTANCE ATTRIBUTES**
+
+    *SourceBuffEdSim* only_buffer -- THE buffer
+    
+    *STR* only_buffer_name -- its name 
+
+
+    *none* --
+    
+    **CLASSS ATTRIBUTES**
+    
+    *none* -- 
+    """
+    buffer_methods = AppState.AppState.buffer_methods[:]
+    buffer_methods.append('print_buff')
+    
+    def __init__(self, **attrs):
+        self.deep_construct(EdSim, {'only_buffer': None,
+	    'only_buffer_name': ""}, attrs)
+	self.only_buffer = SourceBuffEdSim(app = self, file_name = "",
+	    language =None)
+        self.open_buffers[self.only_buffer_name] = self.only_buffer
+
+    def curr_buffer_name(self):
+
+	"""return the name of the current buffer
+
+        **OUTPUTS**
+
+	*STR*  -- current buffer
+	"""
+	return self.only_buffer_name
+    
+    def open_file(self, name, lang=None):
+        """Open a file.
+
+        Open file with name *STR name* and written in language *STR lang*.        
+        """
+        try:
+            source_file = open(name, 'rw')
+            source = source_file.read()
+            source_file.close()
+        except Exception, err:
+            source = ''
+	if self.curr_buffer_name() != None:
+	    del self.open_buffers[self.curr_buffer_name()]
+
+
+        self.only_buffer =  SourceBuffEdSim(app = self, file_name=name, language=lang, \
+	    initial_contents = source)
+	self.only_buffer_name = name
+
+        self.open_buffers[name] = self.only_buffer
+
+    def bidirectional_selection(self):
+      	"""does editor support selections with cursor at left?
+
+	**INPUTS**
+
+	*none*
+
+	**OUTPUTS**
+	
+	*BOOL* -- true if editor allows setting the selection at the
+	left end of the selection"""
+	return 0
+
+
+
+
---
+++ tags/vcode-0-0-46-curr-buffer-method/Mediator/SourceBuffState.py
@@ -0,0 +1,96 @@
+##############################################################################
+# VoiceCode, a programming-by-voice environment
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# (C)2000, David C. Fox
+#
+##############################################################################
+
+"""Basic implementation of a restore-able SourceBuff state"""
+
+import debug
+import re, string, sys
+
+from Object import Object
+from SourceBuff import SourceBuffCookie
+
+class SourceBuffState(SourceBuffCookie):
+    """Basic implementation of a restore-able SourceBuff state
+
+    **CLASS ATTRIBUTES**
+
+    *none*
+
+    **INSTANCE ATTRIBUTES**
+
+    *STR* file_name -- filename of buffer
+
+    *STR* text -- copy of the buffer contents
+
+    *(INT, INT)* selection_range -- range of the selection
+    
+    """
+    
+    def __init__(self, file_name, contents, selection, **attrs):
+        self.deep_construct(SourceBuffState,
+                            {'text': contents,
+			    'file_name': file_name,
+			    'selection_range': selection},
+                            attrs
+                            )
+    def name(self):
+	"""returns file_name
+
+	**INPUTS**
+
+	*none*
+	
+	**OUTPUTS**
+
+	*STR* -- file name
+	"""
+	return self.file_name
+
+    def get_selection(self):
+	"""retrieves range of stored selection.  
+
+	**INPUTS**
+
+	*none*
+	
+	**OUTPUTS**
+
+	*INT* (start, end)
+
+	start is the offset into the buffer of the start of the current
+	selection.  end is the offset into the buffer of the character 
+	following the selection (this matches Python's slice convention).
+	"""
+	return self.selection_range
+
+    def contents(self):
+	"""returns stored contents
+
+	**INPUTS**
+
+	*none*
+
+	**OUTPUTS**
+
+	*STR* -- contents of the buffer
+	"""
+	return self.text
+      
