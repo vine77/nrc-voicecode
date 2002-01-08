0c253503942fda2b0bd73b6b7ddbb79344de30a5
nobody <nobody@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2002-01-08 17:27:59 +0000

    This commit was manufactured by cvs2svn to create tag 'vcode-0-0-59-minor-waxedsim-bug-fix'.

---
+++ tags/vcode-0-0-59-minor-waxedsim-bug-fix/Mediator/SourceBuffEdSim.py
@@ -0,0 +1,508 @@
+##############################################################################
+# VoiceCode, a programming-by-voice environment
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# (C)2000, National Research Council of Canada
+#
+##############################################################################
+
+
+import re, string, sys
+
+from Object import Object
+import find_difference, sb_services, SourceBuffNonCached
+
+#
+# default value of window_size (unless overridden by the constructor argument)
+#
+print_window_size = 3
+
+
+class SourceBuffEdSim(SourceBuffNonCached.SourceBuffNonCached):
+    """concrete class representing a disconnected source buffer for the
+    editor simulator EdSim.
+
+    **INSTANCE ATTRIBUTES**
+    
+    *INT pos=0* -- Cursor position (in number of chars) in the buffer
+    *(INT, INT)* selection_range -- the current selection as
+      character offsets into contents.
+    *STR content=None* -- Content of the source buffer
+    *INT window_size* -- When printing the content of the current buffer, 
+     only print this number of lines before and after the current line
+    *BOOL global_selection* -- makes the 'visible' region the whole
+    buffer (useful for regression testing)
+
+    [SB_ServiceLang] *lang_srv* -- Language service used to know the
+    programming language of a source file.
+
+    [SB_ServiceLineManip] *line_srv* -- Service for manipulating lines.
+
+    [SB_ServiceIndent] *indent_srv* -- Code indentation service used by EdSim.
+
+    CLASS ATTRIBUTES**
+
+    *none*
+
+    ..[SB_ServiceLang] file:///./sb_services.SB_ServiceLang.html
+    ..[SB_ServiceLineManip] file:///./sb_services.SB_ServiceLineManip.html"""
+    
+    def __init__(self, indent_level=3, indent_to_curr_level=1, init_pos=0,
+                 init_selection = None, initial_contents="",
+                 window_size = print_window_size, global_selection = 1,
+                 **attrs):
+
+        self.init_attrs({'lang_srv': sb_services.SB_ServiceLang(buff=self),
+                         'line_srv': sb_services.SB_ServiceLineManip(buff=self),
+                         'indent_srv': sb_services.SB_ServiceIndent(buff=self, indent_level=3, indent_to_curr_level = 1)})
+
+        self.deep_construct(SourceBuffEdSim,
+                            {'pos': init_pos, 
+                             'selection': init_selection, 
+                             'content': initial_contents, 
+			     'window_size': window_size, 
+			     'global_selection': global_selection,
+                             'indent_level': indent_level,
+                             'indent_to_curr_level': indent_to_curr_level,
+			     }, 
+                            attrs
+                            )
+
+	self.pos = self.make_within_range(self.pos)
+	if not self.selection:
+	    self.selection = (self.pos, self.pos)
+	s, e = self.get_selection()
+	if (s < e):
+	    self.selection = (self.pos, self.pos)
+
+
+    def file_name(self):
+        return self.app.only_buffer_name
+
+    def language_name(self):
+        """Returns the name of the language a file is written in
+        
+        **INPUTS**
+        
+        *none*
+        
+        **OUTPUTS**
+        
+        *STR* -- the name of the language
+        """
+
+        return self.lang_srv.language_name()
+
+    def cur_pos(self):
+	return self.pos
+
+    def get_selection(self):
+	return self.selection
+    
+    def set_selection(self, range, cursor_at = 1):
+	start, end = self.make_valid_range(range)
+	self.selection = (start, end)
+	self.pos = end
+
+    def get_text(self, start = None, end = None):
+	if start == None:
+	    start = 0
+	if end == None:
+	    end = self.len()
+	start, end = self.make_valid_range((start, end))
+	return self.content[ start: end]
+    
+    def set_text(self, text, start = None, end = None):
+	if start == None:
+	    start = 0
+	if end == None:
+	    end = self.len()
+	start, end = self.make_valid_range((start, end))
+	before = self.content[0:start]
+        after = self.content[end:]
+        self.content = before + text + after
+
+    def get_visible(self):
+	if self.global_selection:
+	    return (0, self.len())
+	top, bottom = self.lines_around_cursor()
+        lines = string.split(self.contents(), '\n')
+	s = 0
+	for line in lines[0: top -1]:
+	    s = s + len(line) + 1
+	e = s
+	for line in lines[top: bottom -1]:
+	    e = e + len(line) + 1
+	e = e + len(lines[bottom])
+	return s, e
+
+    def make_position_visible(self, position = None):
+	target = self.line_num_of(position)
+	top, bottom = self.lines_around_cursor()
+	if target < top:
+	    destination = target +self.window_size
+	elif target > bottom:
+	    destination = target -self.window_size
+	else:
+	    return
+	self.goto_line(destination)
+
+    def line_num_of(self, position = None):
+        """Returns the line number for a particular cursor position
+        
+        **INPUTS**
+        
+        *INT* position -- The position.
+        
+
+        **OUTPUTS**
+        
+        *INT line_num* -- The line number of that position
+        """
+        return self.line_srv.line_num_of(position)
+
+    def number_lines(self, astring, startnum=1):
+        """Assign numbers to lines in a string.
+
+        Used mainly for the purpose of doing a printout of the buffer
+        content around the cursor (usually during regression testing).
+
+        *STR astring* is the string in question.
+
+        *INT startnum* is the number of the first line in *astring*
+        
+        Returns a list of pairs *[(INT, STR)]* where first entry is
+        the line number and the second entry is the line.
+        
+        .. [self.curr_buffer] file:///AppState.AppState.html"""
+
+        return self.line_srv.number_lines(astring, startnum)        
+
+    def len(self):
+	return len(self.content)
+
+    def beginning_of_line(self, pos):
+        """Returns the position of the beginning of line at position *pos*
+        
+        **INPUTS**
+        
+        *INT* pos -- Position for which we want to know the beginning of line.
+        
+
+        **OUTPUTS**
+        
+        *INT* beg_pos -- Position of the beginning of the line
+        """
+        return self.line_srv.beginning_of_line(pos)
+
+
+    def end_of_line(self, pos):
+        """Returns the position of the end of line at position *pos*
+        
+        **INPUTS**
+        
+        *INT* pos -- Position for which we want to know the end of line.
+        
+
+        **OUTPUTS**
+        
+        *INT* end_pos -- Position of the end of the line
+        """
+        return self.line_srv.end_of_line(pos)
+
+    def refresh_if_necessary(self):
+	self.print_buff()
+
+    def refresh(self):
+	self.print_buff()
+    
+    def print_buff(self, from_line=None, to_line=None):
+        """Prints buffer to STDOUT
+        
+        **INPUTS**
+        
+        *INT* from_line = None -- First line to be printed. If *None*, then
+        print *window_size* lines around cursor.
+
+        *INT* to_line = None -- Last line to be printed.
+
+        **OUTPUTS**
+        
+        *none* -- 
+        """
+
+        #
+        # Figure out the first and last line to be printed
+        #
+        if from_line == None:
+           from_line, to_line = self.lines_around_cursor()
+
+#	print '-- SourceBuffEdSim.print_buffer: from_line=%s, to_line=%s' % (from_line, to_line)
+        #
+        # Figure out the text before/withing/after the selection
+        #
+	selection_start, selection_end = self.get_selection()
+
+        before_content = self.get_text(0, selection_start)
+        selection_content = self.get_text(selection_start, selection_end)
+        after_content = self.get_text(selection_end)
+
+	printed = before_content
+	if selection_content == '':
+	    printed = printed + '<CURSOR>'
+	else:
+	    printed = printed + '<SEL_START>'
+	    printed = printed + selection_content
+	    printed = printed + '<SEL_END>'
+	printed = printed + after_content
+
+	lines_with_num = self.number_lines(printed, startnum = 1)
+        
+	if from_line == 1:
+	    sys.stdout.write("*** Start of source buffer ***\n")
+	for aline in lines_with_num[from_line-1:to_line]:
+	    sys.stdout.write('%3i: %s\n' % (aline[0], aline[1]))
+	if to_line == len(lines_with_num):
+	    sys.stdout.write("\n*** End of source buffer ***\n")
+	return
+
+    def lines_around_cursor(self):
+        """Returns the line numbers of lines around cursor
+        
+        **INPUTS**
+        
+        *none* -- 
+        
+
+        **OUTPUTS**
+        
+        *(INT from_line, INT to_line)*
+
+        *INT from_line* -- First line of the window.
+
+        *INT to_line* -- Last line of the window.
+        """
+
+        curr_line = self.line_num_of(self.cur_pos())
+#        print '-- SourceBuffEdSim.lines_around_cursor: self.cur_pos()=%s, curr_line=%s' % (self.cur_pos(), curr_line)
+        from_line = curr_line - self.window_size
+        to_line = curr_line + self.window_size
+	if from_line < 1:
+	    from_line = 1
+	last_line = self.line_num_of(self.len())
+	if to_line > last_line:
+	    to_line = last_line
+        return from_line, to_line
+        
+    def move_relative_page(self, direction=1, num=1):
+        """Moves up or down a certain number of pages
+        
+        **INPUTS**
+        
+        *INT* direction=1 -- If positive, page down. If negative, page up.
+        
+        *INT* num=1 -- Number of pages to move.
+        
+
+        **OUTPUTS**
+        
+        *none* -- 
+        """
+
+        num_lines = 2*self.window_size + 1
+        self.move_relative_line(direction=direction, num=num_lines)
+
+
+    def insert(self, text, range = None):
+        """Replace text in range with 
+        with text
+
+	**INPUTS**
+
+	*STR text* -- new text
+
+	*(INT, INT)* range -- code range to be replaced.  If None,
+	defaults to the current selection.
+
+	**OUTPUTS**
+
+	*none*
+	"""
+
+	if range == None:
+	    range = self.get_selection()
+	range = self.make_valid_range(range)
+	start, end = range
+	before = self.get_text(0,start)
+	after = self.get_text(end)
+	self.content = before + text + after
+	self.goto(start + len(text))
+
+    def insert_indent(self, code_bef, code_after, range = None):
+        """Insert code into source buffer and indent it.
+
+        Replace code in range 
+        with the concatenation of
+        code *STR code_bef* and *str code_after*. Cursor is put right
+        after code *STR bef*.
+
+	**INPUTS**
+
+	*STR* code_bef -- code to be inserted before new cursor location
+        
+	*STR* code_bef -- code to be inserted after new cursor location
+
+	*(INT, INT)* range -- code range to be replaced.  If None,
+	defaults to the current selection.
+
+	**OUTPUTS**
+
+	*none*
+	"""
+
+        self.indent_srv.insert_indent(code_bef, code_after, range)
+
+    def indent(self, range = None):
+        """Indent code in a source buffer region.
+
+	**INPUTS**
+
+	*(INT, INT)* range -- code range to be replaced.  If None,
+	defaults to the current selection.
+
+	**OUTPUTS**
+
+	*none*
+	"""
+
+        self.indent_srv.indent(range)
+
+    def incr_indent_level(self, levels=1, range=None):
+        
+        """Increase the indentation of a region of code by a certain
+        number of levels.
+        
+        **INPUTS**
+        
+        *INT* levels=1 -- Number of levels to indent by.
+        
+        *(INT, INT)* range=None -- Region of code to be indented 
+        
+
+        **OUTPUTS**
+        
+        *none* -- 
+        """
+
+        self.indent_srv.incr_indent_level(levels, range)
+
+    def decr_indent_level(self, levels=1, range=None):
+
+        """Decrease the indentation of a region of code by a certain number
+        of levels.
+        
+        **INPUTS**
+        
+        *STR* levels=1 -- Number of levels to unindent
+
+        *(INT, INT)* range=None -- Start and end position of code to be indent.
+        If *None*, use current selection
+
+        **OUTPUTS**
+        
+        *none* -- 
+        """
+
+        self.indent_srv.decr_indent_level(levels, range)
+        
+
+    def delete(self, range = None):
+        """Delete text in a source buffer range.
+
+	**INPUTS**
+
+	*(INT, INT)* range -- code range to be deleted.  If None,
+	defaults to the current selection.
+
+	**OUTPUTS**
+
+	*none*
+	"""
+	if range == None:
+	    range = self.get_selection()
+	range = self.make_valid_range(range)
+	start, end = range
+	before = self.content[0:start]
+        after = self.content[end:]
+        self.content = before + after
+        self.goto(start)
+
+        
+    def goto(self, pos):
+
+        """Moves the cursor to position *INT pos* of source buffer
+	(and make selection empty) 
+        """
+        
+	pos = self.make_within_range(pos)
+	self.pos = pos
+	self.selection = (pos, pos)
+
+
+
+    def goto_line(self, linenum, where=-1):
+        """Go to a particular line in a buffer.
+
+        *INT linenum* is the line number.
+
+        *INT where* indicates if the cursor should go at the end
+         (*where > 0*) or at the beginning (*where < 0*) of the line.
+	"""
+
+        self.line_srv.goto_line(linenum, where)
+
+    def newline_conventions(self):
+        
+        """Returns a list of the forms of newline the editor can
+        recognise for this buffer.
+        
+        **INPUTS**
+        
+        *none* -- 
+        
+
+        **OUTPUTS**
+        
+        *none* -- 
+        """
+        
+        return ['\n']
+
+
+    def pref_newline_convention(self):
+        """Returns the form of newline that the editor prefers for this buffer.
+        
+        **INPUTS**
+        
+        *none* -- 
+        
+
+        **OUTPUTS**
+        
+        *none* -- 
+        """
+        
+        return '\n'
---
+++ tags/vcode-0-0-59-minor-waxedsim-bug-fix/Mediator/SourceBuffNonCached.py
@@ -0,0 +1,60 @@
+##############################################################################
+# VoiceCode, a programming-by-voice environment
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# (C) 2001, National Research Council of Canada
+#
+##############################################################################
+
+"""Interface to a buffer in a fast programming environment (i.e. no
+need for caching)"""
+
+
+import SourceBuff
+
+class SourceBuffNonCached(SourceBuff.SourceBuff):
+    
+    """Interface to a a buffer in a fast programming environment
+    (i.e. no need for caching)
+
+    This [SourceBuff] subclass should be used in situations when the
+    external editor communicates with VoiceCode through a fast
+    link. In such cases, we can afford to always read the state
+    directly from the external editor.
+
+    **INSTANCE ATTRIBUTES**
+    
+    *none*-- 
+
+    CLASS ATTRIBUTES**
+    
+    *none* -- 
+
+    ..[SourceBuff] file:///./SourceBuff.SourceBuff.html"""
+    
+    def __init__(self, **args_super):
+        self.deep_construct(SourceBuffNonCached, 
+                            {}, 
+                            args_super, 
+                            {})
+
+    def apply_this_update(self, an_update):
+        
+        """Because we always read the state directly from the editor,
+        we don't need to do anything with the updates.        
+        """
+        
+        pass
