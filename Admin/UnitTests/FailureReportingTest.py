ede08a5cfece6860c23c8ec713ba81832aad0949
nobody <nobody@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2002-06-21 14:08:41 +0000

    This commit was manufactured by cvs2svn to create tag 'vcode-0-0-100-server-main-thread-abstract'.

--- Admin/UnitTests/FailureReportingTest.py
+++
@@ -1,7 +0,0 @@
-class FailureReportingTest(TestCase):
-    def test_failure_reporting(self):
-        assert 0, 'NEVER MIND THIS FAILURE!\nJust testing that PyUnit reports failures.'
-
-
-test_runner.add_suite(FailureReportingTest)
-                              
