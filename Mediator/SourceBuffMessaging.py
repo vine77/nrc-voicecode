b01f1744c648c60b225ddeb94cdaf32fb9ae436c
dcfox <dcfox@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2002-07-26 17:38:42 +0000

    - another test commit, although I did add some information to the   docstrings of SourceBuffMessaging.SourceBuffInsertIndentMess and the   corresponding class in AppStateMessaging.py - forgot to update the docs for those file

--- Mediator/SourceBuffMessaging.py
+++ Mediator/SourceBuffMessaging.py
@@ -546,8 +546,15 @@ class SourceBuffInsertIndentMess(SourceBuffMessaging):
     SourceBuff implementation of insert_indent in terms of insert and
     indent.
 
-    This is mostly used for external versions of the dumb test editors,
-    for which the generic implementation is not adequate.
+    **NOTE:** This class is used only for test editors.  Real editors 
+    supporting client-side indentation should use SourceBuffMessaging.  
+    Real editors not supporting client-side indentation should use 
+    server-side indentation (see SB_MessExtEdSim in tcp_server.py 
+    for an example).
+
+    Its purpose is to work with clients with an incomplete implementation 
+    of client-side indentation which won't work with the generic 
+    AppState.insert_indent, because indent is implemented as a no-op.
     
     **INSTANCE ATTRIBUTES**
 
