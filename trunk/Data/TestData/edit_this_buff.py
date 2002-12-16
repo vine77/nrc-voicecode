eb90aaf83a832d8fb3d1cd665353340e5ed0905d
alain_desilets <alain_desilets@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2002-12-16 17:56:28 +0000

    - Exiting Emacs by clicking on the X doesn't crash the server anymore.

---
+++ trunk/Data/TestData/edit_this_buff.py
@@ -0,0 +1,25 @@
+# This is a small buffer for testing editing of Python code
+
+
+class AClass(ASuper):
+    """This is a dummy class"""
+    
+    def a_method(self, x):
+        some_variable = some_array[0]
+        if some_variable:
+           do_some_stuff(some_array)
+           do_some_more()
+           
+        try:
+           do_some_stuff()
+
+    def a_method_with_no_arguments():
+        pass
+           
+
+
+
+class SomeOtherClass():
+
+	def some_other_method(self):
+	    pass
