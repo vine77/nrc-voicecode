7b1edfca53590b5ffa41da0b0effbb74e07d389d
nobody <nobody@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2003-05-13 16:13:24 +0000

    This commit was manufactured by cvs2svn to create tag 'vcode-1-0-62-retag'.

---
+++ tags/vcode-1-0-62-retag/Config/user_abbrevs.py
@@ -0,0 +1,55 @@
+##############################################################################
+# VoiceCode, a programming-by-voice environment
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# (C)2000, National Research Council of Canada
+#
+##############################################################################
+
+###############################################################################
+# User added abbreviations
+###############################################################################
+
+# from MediatorObject import add_abbreviation
+
+add_abbreviation('attr', ['attribute'])
+add_abbreviation('buff', ['buffer'])
+add_abbreviation('cmd', ['command'])
+add_abbreviation('cpp', ['C. plus plus'])
+add_abbreviation('arg', ['argument'])
+add_abbreviation('cond', ['condition'])
+add_abbreviation('cont', ['control'])
+add_abbreviation('curr', ['current'])
+add_abbreviation('def', ['definition', 'default', 'define', 'defined', 'deaf'])
+add_abbreviation('dict', ['dictionary'])
+add_abbreviation('environ', ['environment'])
+add_abbreviation('exc', ['exception'])
+add_abbreviation('gen', ['general', 'generic'])
+add_abbreviation('getattr', ['get attribute'])
+add_abbreviation('horiz', ['horizontal', 'horizontally'])
+add_abbreviation('inc', ['increment', 'include'])
+add_abbreviation('init', ['initial', 'initialize'])
+add_abbreviation('interp', ['interpreter'])
+add_abbreviation('mtime', ['M. time'])
+add_abbreviation('os', ['operating system', 'O. S.'])
+add_abbreviation('pos', ['position', 'positioning'])
+add_abbreviation('prof', ['profile', 'profiling', 'professional'])
+add_abbreviation('py', ['python'])
+add_abbreviation('regexp', ['regular expression'])
+add_abbreviation('st', ['standard'])
+add_abbreviation('sys', ['system'])
+add_abbreviation('undef', ['undefined'])
+add_abbreviation('vert', ['vertical', 'vertically'])
