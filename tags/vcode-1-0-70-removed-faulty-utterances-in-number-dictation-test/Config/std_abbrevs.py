fb5d4df4dc4fb7015ba2a4d9d9268971fd9d9e85
nobody <nobody@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2003-06-04 20:17:31 +0000

    This commit was manufactured by cvs2svn to create tag 'vcode-1-0-70-removed-faulty-utterances-in-number-dictation-test'.

---
+++ tags/vcode-1-0-70-removed-faulty-utterances-in-number-dictation-test/Config/std_abbrevs.py
@@ -0,0 +1,70 @@
+
+##############################################################################
+# VoiceCode, a programming-by-voice environment
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# (C)2000, National Research Council of Canada
+#
+##############################################################################
+
+
+add_alt_abbreviations('argument', ['arg'])
+add_alt_abbreviations('attribute', ['attr'])
+add_alt_abbreviations('buffer', ['buff'])
+add_alt_abbreviations('C. plus plus', ['cpp'])
+add_alt_abbreviations('command', ['cmd'])
+add_alt_abbreviations('condition', ['cond'])
+add_alt_abbreviations('control', ['cont'])
+add_alt_abbreviations('current', ['curr'])
+# should really be just an expansion
+add_alt_abbreviations('deaf', ['def'])
+add_alt_abbreviations('default', ['def'])
+add_alt_abbreviations('define', ['def'])
+add_alt_abbreviations('defined', ['def'])
+add_alt_abbreviations('definition', ['def'])
+add_alt_abbreviations('dictionary', ['dict'])
+add_alt_abbreviations('environment', ['environ'])
+add_alt_abbreviations('exception', ['exc'])
+add_alt_abbreviations('general', ['gen'])
+add_alt_abbreviations('generic', ['gen'])
+# belongs in py_std_abbrevs
+add_alt_abbreviations('get attribute', ['getattr'])
+add_alt_abbreviations('horizontal', ['horiz'])
+add_alt_abbreviations('horizontally', ['horiz'])
+add_alt_abbreviations('include', ['inc'])
+add_alt_abbreviations('increment', ['inc'])
+add_alt_abbreviations('initial', ['init'])
+add_alt_abbreviations('initialize', ['init'])
+add_alt_abbreviations('interpreter', ['interp'])
+# py_std
+add_alt_abbreviations('M. time', ['mtime'])
+add_alt_abbreviations('O. S.', ['os'])
+add_alt_abbreviations('operating system', ['os'])
+add_alt_abbreviations('position', ['pos'])
+add_alt_abbreviations('positioning', ['pos'])
+add_alt_abbreviations('professional', ['prof'])
+add_alt_abbreviations('profile', ['prof'])
+add_alt_abbreviations('profiling', ['prof'])
+# py_std
+# also add py -> pie?
+# but not the reverse
+add_alt_abbreviations('python', ['py'])
+add_alt_abbreviations('regular expression', ['regexp'])
+add_alt_abbreviations('standard', ['st'])
+add_alt_abbreviations('system', ['sys'])
+add_alt_abbreviations('undefined', ['undef'])
+add_alt_abbreviations('vertical', ['vert'])
+add_alt_abbreviations('vertically', ['vert'])
