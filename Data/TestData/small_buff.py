fb48533afc6433a70039646b7cc82be33df563c2
nobody <nobody@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2004-07-06 17:58:01 +0000

    This commit was manufactured by cvs2svn to create tag 'vcode-1-0-163-symbol-reformatting-now-functionnal'.

--- Data/TestData/small_buff.py
+++
@@ -1,9 +0,0 @@
-# This is a small test buffer for Python
-
-
-
-class AClass(ASuper):
-    """This is a dummy class"""
-    
-    def a_method(self, x):
-        print '... and this is a dummy class'
