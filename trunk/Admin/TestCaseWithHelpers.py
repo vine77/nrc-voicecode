c6de1beead6c49f327a9a7e8a8e3e4a7e11a9f51
alain_desilets <alain_desilets@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2006-12-01 14:03:59 +0000

    - Qintijn's early work on what_can_i_say().

--- trunk/Admin/TestCaseWithHelpers.py
+++ trunk/Admin/TestCaseWithHelpers.py
@@ -16,6 +16,16 @@ class TestCaseWithHelpers(unittest.TestCase):
         self.assert_(string.find(the_string, pattern) != -1, 
                      mess + "\nSubstring: '%s' was not found in string: '%s'" % (pattern, the_string))
         
+    def _display_both_sequences_mess(self, expected, got):
+        return "\nExpected sequence:\n   %s\nGot sequence:\n   %s" % (repr(expected), repr(got))
+
+        
+    def assert_sequences_have_same_length(self, expected, got, mess):
+        display_both_lists_mess = self._display_both_sequences_mess(expected, got)
+        
+        self.assert_equals(len(expected), len(got), 
+                           mess + "\nSequences did not have the same length." +
+                           display_both_lists_mess)
 
     def assert_sequences_have_same_content(self, expected, got, mess):
         """Use this to compare long lists, instead of just doing:
@@ -26,8 +36,7 @@ class TestCaseWithHelpers(unittest.TestCase):
         the index of the first entry that differs, so you won't have to scan two
         long lists visually.
         """
-        display_both_lists_mess = \
-           "\nExpected list:\n   %s\nGot list:\n   %s" % (repr(expected), repr(got))
+        display_both_lists_mess = self._display_both_sequences_mess(expected, got)
         
         if len(expected) != len(got):  
            self.fail(mess + "\nExpeted sequence of length %s, but got sequence of length %s" 
@@ -39,3 +48,10 @@ class TestCaseWithHelpers(unittest.TestCase):
               self.fail(mess + "\nItem at position %s was wrong. Expected '%s', but got '%s'" \
                      % (ii, expected[ii], got[ii]))
            
+    def assert_dicts_have_same_keys(self, expected, got, mess):
+        expected_keys = expected.keys()
+        expected_keys.sort()
+        got_keys = got.keys()
+        got_keys.sort()
+        self.assert_sequences_have_same_content(expected_keys, got_keys, 
+                                                mess + "\nList of keys for dictionaries was not as expected.")
