2bf5f673e0edda4bb4ae4a5362e6db6e90906cd0
quintijn <quintijn@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2007-01-09 12:36:46 +0000

    small change, cosmetical, Quintijn

--- Admin/SourceBuffTest.py
+++ Admin/SourceBuffTest.py
@@ -7,11 +7,9 @@ class SourceBuffTest(VoiceCodeRootTest.VoiceCodeRootTest):
         VoiceCodeRootTest.VoiceCodeRootTest.__init__(self, name)
     
     def setUp(self):
-      self._init_simulator_regression(exclusive=0)
-      self._open_empty_test_file('blah.py')
-      self.source_buff = self._app().curr_buffer()
-      self.source_buff.insert("line1\nline2\nline3\nline4")
-      self.source_buff.goto(0)
+       self._init_simulator_regression(exclusive=0)
+       self._open_empty_test_file('blah.py')
+       self.source_buff = self._app().curr_buffer()
                                                          
     def tearDown(self):
         pass
@@ -28,8 +26,12 @@ class SourceBuffTest(VoiceCodeRootTest.VoiceCodeRootTest):
 #
 # These tests check the internal workings of the class.
 ##########################################################
-
+       
     def test_get_text_of_line(self):
+
+       # setting up 4 lines:
+       self.source_buff.insert("line1\nline2\nline3\nline4")
+       self.source_buff.goto(0)
        self.source_buff.goto_line(0)
        got_text = self.source_buff.get_text_of_line(3)
        self.assert_equal("line3", got_text, 
@@ -56,7 +58,34 @@ class SourceBuffTest(VoiceCodeRootTest.VoiceCodeRootTest):
        got_text = self.source_buff.get_text_of_line(-1)
        self.assert_equal("line1", got_text, 
                          "Asking for content of line before first line, should return content of first line.")
-      
+
+
+
+    def test_get_text_of_previous_lines(self):
+
+       # setting up 4 lines:
+       self.source_buff.insert("line1\nline2\nline3\nline4")
+       self.source_buff.goto_line(3)
+
+       current_pos = self.source_buff.cur_pos()
+       num = self.source_buff.line_num_of(current_pos)
+       self.assert_equal(3, num, "Got wrong line number")
+       got_text = self.source_buff.get_text_of_line(num)
+       self.assert_equal("line3", got_text, 
+                         "Got wrong content of line num.")
+
+       Lines = []
+       for i in range(num, 0, -1):
+           Lines.insert(0, self.source_buff.get_text_of_line(i))
+       got_text = '\n'.join(Lines)
+       self.assert_equal(got_text, "line1\nline2\nline3",
+                         "Got wrong content of lines 1,2,3")
+
+       check_current_pos = self.source_buff.cur_pos()
+       self.assert_equal(current_pos, check_current_pos,
+                         "Current position should not have changed")
+       
+
       
 ###############################################################
 # Assertions.
