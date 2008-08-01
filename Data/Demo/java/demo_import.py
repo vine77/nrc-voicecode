5144e7759adbd9f520f8b14dec7c5c3b44fe4738
quintijn <quintijn@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2008-08-01 07:56:28 +0000

    Setting up testing acceptance tests, JavaScript, Java

---
+++ Data/Demo/java/demo_import.py
@@ -0,0 +1,9 @@
+# demo file generated with mediator test of JavaAcceptanceTest
+#
+utt1 = "import hello world"
+exp1 = \
+"import module_hello_world<CURSOR>"
+utt2 = "new statement"
+exp2 = \
+"""import module_hello_world<CURSOR>;
+    """
--- Data/Demo/javascript/demo_define_variable.py
+++ Data/Demo/javascript/demo_define_variable.py
@@ -1,37 +1,21 @@
 # demo file generated with mediator test of JavascriptAcceptanceTest
 #
-name = "define variable" 
-utt1 = 'new statement'
-exp1 = \
-"""<CURSOR>;
-"""
-#
 utt2 = "define variable"
 exp2 = \
-"""define_variable<CURSOR>;
-"""
-#
+"var <CURSOR>;"
 utt3 = "example equals zero"
 exp3 = \
-"""define_variableexample = 0<CURSOR>;
-"""
-#
+"var example = 0<CURSOR>;"
 utt4 = "new statement"
 exp4 = \
-"""define_variableexample = 0;
-<CURSOR>;
+"""var example = 0;<CURSOR>;
 """
-#
 utt5 = "india plus plus"
 exp5 = \
-"""define_variableexample = 0;
-i++<CURSOR>;
+"""var example = 0;i++<CURSOR>;
 """
-#
 utt6 = "new statement"
 exp6 = \
-"""define_variableexample = 0;
-i++;
+"""var example = 0;i++;
 <CURSOR>;
 """
-#
