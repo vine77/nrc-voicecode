c0f08344bc8e79222e860890f79105dde08e4b32
william-r-s <william-r-s@44abcc16-4449-0410-9dcd-83bacbc7a39c>
2011-07-10 05:03:59 +0000

    merged into trunk

--- branches/dragon11/Data/Demo/java/demo_import.py
+++
@@ -1,7 +0,0 @@
-# demo file generated with mediator test of JavaAcceptanceTest
-#
-utt1 = "import hello world"
-exp1 = "import module_hello_world<CURSOR>"
-
-utt2 = "new statement"
-exp2 = "import module_hello_world<CURSOR>;"
--- branches/dragon11/Environments/Emacs/README.txt
+++
@@ -1,4 +0,0 @@
-This directory contains files for emacs.
-
--setnu.el is actually not used by voicecode, but it is my unimacro commands, switching on or off line numbers.
-
